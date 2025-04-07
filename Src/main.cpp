ebce34117e7c900fb077d2bfa4a2a857c439ae22
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-04-07 10:51:31 +0200

    third version

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,14 +1,7 @@
 #include <Arduino.h>
-
-int temperaturePin = A0; // Analog pin A0
-int temperatureSensorValue = 0; // later converted into voltage
-float temperatureCelsius = 0.0;
-
-int pirPin = A1; // Analog pin A1
-int pirMovementValue = 0;
-
-int gasPin = A2;
-float gasLevel = 0; 
+#include "temperatureSensor.h" 
+#include "movementSensor.h"
+#include "gasSensor.h"
 
 void setup()
 {
@@ -18,35 +11,8 @@ void setup()
 
 void loop()
 {
-
+  gasSensor();
+  temperatureSensor();
+  movementSensor();
 }
 
-
-// handles logic related to the temperature sensor
-void temperatureSensor()
-{
-  temperatureSensorValue = analogRead(temperaturePin);
-  float voltage = temperatureSensorValue * (5.0 / 1023.0); 
-  temperatureCelsius = (voltage - 0.5) * 100.0;
-  
-  Serial.print("Temperature in Celcius: ");
-  Serial.println(temperatureCelsius);
-}
-
-
-// handles logic related to the movement sensor
-void movementSensor()
-{
-  pirMovementValue = analogRead(pirPin);
-  
-  Serial.print("Movement Value: ");
-  Serial.println(pirMovementValue);
-}
-
-// handles logic related to the gas sensor
-void gasSensor()
-{ 
-	gasLevel = analogRead(gasPin); 
-	Serial.print("Gas Level: ");
-	Serial.println(gasLevel);  
-}

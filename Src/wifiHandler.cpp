2a18fd1d0f13744299be73237158c1446dfb1283
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-04-14 10:11:57 +0200

    Third Version

---
+++ Src/wifiHandler.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include "config.h"
+#include <WiFiS3.h>
+#include "wifiHandler.h"
+
+
+void connectToWiFi(unsigned long timeout = 10000) {
+    Serial.print("Connecting to WiFi");
+
+    int status = WL_IDLE_STATUS;
+    unsigned long startAttemptTime = millis();
+
+// keeps looping only if the elapsed time is less than the timeout, if the wifi hasnt connected
+    while (status != WL_CONNECTED && millis() - startAttemptTime < timeout) {
+        status = WiFi.begin(ssid, password);
+        delay(1000);
+        Serial.print(".");
+    }
+
+    if (status == WL_CONNECTED) {
+        Serial.println("\nConnected to WiFi!");
+        Serial.print("IP Address: ");
+        Serial.println(WiFi.localIP());
+    } else {
+        Serial.println("\n WiFi not connected — continuing without connection.");
+    }
+}
